688bf2c3f06ab9c45cb369bdff45ee9df7fa2a6f
root <root@localhost.localdomain>
2017-04-07 10:06:52 -0300

    test2

--- challenge/caso2/generartrafico.py
+++ challenge/caso2/generartrafico.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 
 
-testing_host=["127.0.0.1","google.com","googlnada.com"]
+testing_host=["127.0.0.1","google.com","github.com","googlnada.com"]
 while True:
     for host in testing_host:
        rest = subprocess.Popen(["fping",host],stdout=subprocess.PIPE).stdout.read()
--- challenge/caso2/tcpdump.sh
+++ challenge/caso2/tcpdump.sh
@@ -1,5 +1,5 @@
 while [ true ]
 do
-    timeout 60s tcpdump eth0 -nnnt dst port 53 -w datos.pcap
-    tcpdump eth0 -nnnt dst port 53 -r datos.pcap |echo $(date +%H:%M) $(wc -l) >> data.tsv
+    timeout 60s tcpdump  -nnnt dst port 53 -w datos.pcap
+    tcpdump  -nnnt dst port 53 -r datos.pcap |echo $(date +%H:%M) $(wc -l) >> data.tsv
 done
